0f09b4cf13a693a6d63d9d66444a6f6b0c228b3f
VasulShahala <vasyl.shahala.kn.2015@lpnu.ua>
2017-08-08 20:14:00 +0300

    Revert "Revert "LvDU""
    This reverts commit 726c949523f8c00777d4242eab4e582ee85bf9a7.

---
+++ WebAppLvivDebateUnion/WebAppLvivDebateUnion/Models/Rolle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppLvivDebateUnion.Models
+{
+    public class Rolle
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        //Referense to users syst
+        public List<Debater> Debaters { get; set; }
+        
+    }
+}
